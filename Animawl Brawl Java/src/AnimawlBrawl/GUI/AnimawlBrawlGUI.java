1f61dc8e97b3ee41fa5941d7a28a08c53ee802db
Will Mateer <wmateer@umail.ucsb.edu>
2013-10-10 01:19:00 -0700

    Added some functionality to log in screen.  Still has some bugs that need to be ironed out.  Two in particular, I'll let you guys know later.

--- Animawl Brawl Java/src/AnimawlBrawl/GUI/AnimawlBrawlGUI.java
+++ Animawl Brawl Java/src/AnimawlBrawl/GUI/AnimawlBrawlGUI.java
@@ -3,12 +3,16 @@ package AnimawlBrawl.GUI;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.ArrayList;
+import java.util.*;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.swing.event.*;
+
+import java.io.*;
+import javax.imageio.ImageIO;
 
 /**
  Main Class that produces a functional login screen
@@ -21,12 +25,31 @@ public class AnimawlBrawlGUI {
 	private JLabel loginText;
 	private JLabel passwordText;
 	private JTextField loginTextField;
-	private JTextField passwordTextField;
+	private JPasswordField passwordTextField; //needs to be password field
 	private JPanel mainPanel;
+	private HashMap <String,String> storedUserData;
+
+	private JLabel loginReg;
+	private JLabel regPassTextFirst;
+	private JLabel regPassTextSecond;
+	private JTextField regLogTextField;
+	private JPasswordField regPassFirstTextField;
+	private JPasswordField regPassSecondTextField;
+	private JPanel regPanel;
 	
 	public AnimawlBrawlGUI() {
+		initGUI();
+	}
+
+	private void initGUI() {
+				
+		//retrieve stored user data to storedUserData
 		
-	
+		//storedUserData = new HashMap<String,String>();
+		//storedUserData.put("will","cs");
+		
+		CheckSavedUserData();
+		
 		mainFrame = new JFrame("Login Screen For Animawl Brawl");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
@@ -49,7 +72,7 @@ public class AnimawlBrawlGUI {
 		passwordText.setFont(new Font("SansSerif", Font.BOLD, 20));
 		
 		loginTextField = new JTextField(20);
-		passwordTextField = new JTextField(20);
+		passwordTextField = new JPasswordField(20);
 		
 		JButton loginButton = new JButton("LOGIN");
 		loginButton.addActionListener(new loginListener());
@@ -77,29 +100,214 @@ public class AnimawlBrawlGUI {
         mainFrame.setVisible(true);
 	}
 	
+	private void CheckSavedUserData() {
+		//checks the saved Serialzed obj file.
+		try {
+			FileInputStream fileStream = new FileInputStream("build/Saved_User_and_Passwords.ser");
+			ObjectInputStream inputStream = new ObjectInputStream(fileStream);
+			Object UserDefaults = inputStream.readObject();
+			HashMap<String,String> TmpDefault = (HashMap<String,String>) UserDefaults;
+			storedUserData.putAll(TmpDefault);
+			inputStream.close();
+		}
+		catch (Exception ex) {
+			DefaultUserViaSerializable();
+			System.out.println("Saved_User_and_Passwords.ser does not exist.  This means there are no previous users registered.");
+			
+			/*try{
+				FileOutputStream fileStream = new FileOutputStream("build/Saved_User_and_Passwords.ser");
+				ObjectOutputStream outStream = new ObjectOutputStream(fileStream);
+				HashMap<String,String> TmpMap = new HashMap<String,String>();
+				TmpMap.put("will","cs");
+				outStream.writeObject(TmpMap);
+				outStream.close();
+			}
+			catch (Exception sex) {
+				ex.printStackTrace();
+			}*/
+		}
+	}
+	
+	private void DefaultUserViaSerializable() { 
+		try{
+			FileOutputStream fileStream = new FileOutputStream("build/Saved_User_and_Passwords.ser");
+			ObjectOutputStream outStream = new ObjectOutputStream(fileStream);
+			storedUserData = new HashMap<String,String>();
+			storedUserData.put("will","cs");
+			
+			outStream.writeObject(storedUserData);
+			outStream.close();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	private void SaveNewUserViaSerializable() { 
+		try{
+			FileOutputStream fileStream = new FileOutputStream("build/Saved_User_and_Passwords.ser");
+			ObjectOutputStream outStream = new ObjectOutputStream(fileStream);
+			outStream.writeObject(storedUserData);
+			outStream.close();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
 	class loginListener implements ActionListener {
 		public void actionPerformed(ActionEvent event){
 			
-			loginTextField.setText("BUTTON WORKS");
-			//if ( checkDatabaseBoth(loginText.getText(),passwordText.getText()) ) {
-			//	login as user; }
-			//else {
-			// login is incorrect;}
-			//frame.setVisible(false);
-			//frame.dispose();
-			//>> load up main menu of animawl brawl <<
+			//loginTextField.setText("BUTTON WORKS");
+			String inputPassword = new String(passwordTextField.getPassword());
+			
+			String check = storedUserData.get(loginTextField.getText());
+			if (check.equals(inputPassword)) {
+				mainFrame.setVisible(false);
+				//registerFrame.dispose();
+				//DISPLAY SUCCESS PICTURE
+				mainFrame.getContentPane().removeAll();
+				try {
+					File file = new File("src/AnimawlBrawl/GUI/success.jpg");
+					BufferedImage image = ImageIO.read(file);
+					JLabel label = new JLabel(new ImageIcon(image));
+					mainFrame.getContentPane().add(label);
+					mainFrame.pack();
+					mainFrame.setLocation(200,200);
+					mainFrame.setVisible(true);
+				}
+				catch (Exception ex) {
+					System.out.println("Success.jpg does not exist.");
+				}
+			}
+			else if (!(check.equals(null)) && !(check.equals(inputPassword))) {
+				JOptionPane.showMessageDialog(null, "PASSWORD IS INCORRECT", "PASSWORD IS INCORRECT", JOptionPane.ERROR_MESSAGE);
+			}
+			else if (!storedUserData.containsKey(loginTextField.getText())) {
+				JOptionPane.showMessageDialog(null, "USERNAME DOES NOT EXIST", "USERNAME DOES NOT EXIST", JOptionPane.ERROR_MESSAGE);
+			}
 		}
 	}
 	
 	class registerListener implements ActionListener {
 		public void actionPerformed(ActionEvent event){
-			
-			passwordTextField.setText("BUTTON WORKS");
-			//checkDatabaseUser(loginText.getText());
-			//confirmPassword(PASSWORD);
-			//storeNewUser(USER,PASSWORD);
+				//JFrame registerFrame = new JFrame();
+				mainFrame.setVisible(false);
+				mainFrame.getContentPane().removeAll();
+				
+				//registerFrame = new JFrame("Login Screen For Animawl Brawl");
+				//registerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						
+				loginReg = new JLabel("USER NAME",JLabel.CENTER);
+				loginReg.setForeground(Color.black);
+				loginReg.setBackground(Color.white);
+				loginReg.setOpaque(true);
+				loginReg.setFont(new Font("SansSerif", Font.BOLD, 20));
+						
+				regPassTextFirst = new JLabel("PASSWORD",JLabel.CENTER);
+				regPassTextFirst.setForeground(Color.black);
+				regPassTextFirst.setBackground(Color.white);
+				regPassTextFirst.setOpaque(true);
+				regPassTextFirst.setFont(new Font("SansSerif", Font.BOLD, 20));
+				
+				regPassTextSecond = new JLabel("RETYPE PASSWORD",JLabel.CENTER);
+				regPassTextSecond.setForeground(Color.black);
+				regPassTextSecond.setBackground(Color.white);
+				regPassTextSecond.setOpaque(true);
+				regPassTextSecond.setFont(new Font("SansSerif", Font.BOLD, 20));
+						
+				regLogTextField = new JTextField(20);
+				regPassFirstTextField = new JPasswordField(20);
+				regPassSecondTextField = new JPasswordField(20);
+						
+				JButton newUserButton = new JButton("REGISTER");
+				newUserButton.addActionListener(new newUserListener());
+						
+				JButton backButton = new JButton("BACK");
+				backButton.addActionListener(new backListener());
+						
+				regPanel = new JPanel(new GridLayout(0,2));
+						
+				regPanel.add(loginReg);
+				regPanel.add(regLogTextField);
+				regPanel.add(regPassTextFirst);
+				regPanel.add(regPassFirstTextField);
+				regPanel.add(regPassTextSecond);
+				regPanel.add(regPassSecondTextField);
+				regPanel.add(newUserButton);
+				regPanel.add(backButton);
+				regPanel.setPreferredSize(new Dimension(400,300));
+						
+				mainFrame.add(regPanel);
+				mainFrame.pack();
+				
+				mainFrame.setVisible(true);
+				//registerFrame.setVisible(true);
+			}
 		}
-	}
+		
+		class newUserListener implements ActionListener {
+			public void actionPerformed(ActionEvent event){
+			
+				String regUserName = regLogTextField.getText();
+				String regPasswordFirst = new String(regPassFirstTextField.getPassword());
+				String regPasswordSecond = new String(regPassSecondTextField.getPassword());
+				
+				if (regPasswordFirst.equals(regPasswordSecond)) {
+					//check if regUserName is in datafile
+					
+					
+					String check = storedUserData.get(regUserName);
+					if (check == null) {
+						storedUserData.put(regUserName,regPasswordFirst);
+						SaveNewUserViaSerializable();
+						CheckSavedUserData();
+						//registerFrame.setVisible(false);
+						mainFrame.setVisible(false);
+						//registerFrame.dispose();
+						mainFrame.getContentPane().removeAll();
+						initGUI();
+						JOptionPane.showMessageDialog(null, "REGISTRATION SUCCESSFUL, PLEASE LOGIN", "REGISTRATION SUCCESSFUL, PLEASE LOGIN", JOptionPane.ERROR_MESSAGE);
+						
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "USERNAME ALREADY IN USE", "USERNAME ALREADY IN USE", JOptionPane.ERROR_MESSAGE);
+					}
+
+					//if it is then pop up box telling that username is already taken
+					//if it is not already taken then pop up box telling that UserName registered successfully
+					//--and return to main log in screen
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "PASSWORDS DO NOT MATCH", "PASSWORDS DO NOT MATCH", JOptionPane.ERROR_MESSAGE);
+					//regPassFirstTextField = new JPasswordField(20);
+					//regPassSecondTextField = new JPasswordField(20);
+					//display pop in text box that shows different
+				}
+
+				
+				//store userlist in an arraylist and use find on it
+				//if ( checkDatabaseBoth(loginText.getText(),passwordText.getText()) ) {
+				//	login as user; }
+				//else {
+				// login is incorrect;}
+				//frame.setVisible(false);
+				//frame.dispose();
+				//>> load up main menu of animawl brawl <<
+			}
+		}
+		
+		class backListener implements ActionListener {
+			public void actionPerformed(ActionEvent event){
+				
+				//registerFrame.setVisible(false);
+				mainFrame.setVisible(false);
+				mainFrame.getContentPane().removeAll();
+				initGUI();
+			}
+		}
 	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -113,3 +321,91 @@ public class AnimawlBrawlGUI {
 	
 	
 }
+
+
+
+
+
+
+
+
+
+/*
+class registerListener implements ActionListener {
+	public void actionPerformed(ActionEvent event){
+		
+		final Runnable Linker = new Runnable() {
+			public void run() {
+				
+			}
+		};
+		
+		Thread LinkThread = new Thread() {
+			public void run() {
+				try {
+					synchronized (this) {
+						SwingUtilities.invokeAndWait(Linker);
+						
+					}
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				finally {
+					JFrame registerFrame = new JFrame();
+					
+					registerFrame = new JFrame("Login Screen For Animawl Brawl");
+					registerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					
+					loginReg = new JLabel("USER NAME",JLabel.CENTER);
+					loginReg.setForeground(Color.black);
+					loginReg.setBackground(Color.white);
+					loginReg.setOpaque(true);
+					loginReg.setFont(new Font("SansSerif", Font.BOLD, 20));
+					
+					regPassTextFirst = new JLabel("PASSWORD",JLabel.CENTER);
+					regPassTextFirst.setForeground(Color.black);
+					regPassTextFirst.setBackground(Color.white);
+					regPassTextFirst.setOpaque(true);
+					regPassTextFirst.setFont(new Font("SansSerif", Font.BOLD, 20));
+					
+					regPassTextSecond = new JLabel("RETYPE PASSWORD",JLabel.CENTER);
+					regPassTextSecond.setForeground(Color.black);
+					regPassTextSecond.setBackground(Color.white);
+					regPassTextSecond.setOpaque(true);
+					regPassTextSecond.setFont(new Font("SansSerif", Font.BOLD, 20));
+					
+					regLogTextField = new JTextField(20);
+					regPassFirstTextField = new JPasswordField(20);
+					regPassSecondTextField = new JPasswordField(20);
+					
+					JButton newUserButton = new JButton("REGISTER");
+					newUserButton.addActionListener(new newUserListener());
+					
+					JButton backButton = new JButton("BACK");
+					backButton.addActionListener(new backListener());
+					
+					regPanel = new JPanel(new GridLayout(0,2));
+					
+					regPanel.add(loginReg);
+					regPanel.add(regLogTextField);
+					regPanel.add(regPassTextFirst);
+					regPanel.add(regPassFirstTextField);
+					regPanel.add(regPassTextSecond);
+					regPanel.add(regPassSecondTextField);
+					regPanel.add(newUserButton);
+					regPanel.add(backButton);
+					regPanel.setPreferredSize(new Dimension(400,300));
+					
+					registerFrame.add(mainPanel);
+					registerFrame.pack();
+					
+					mainFrame.setVisible(false);
+					registerFrame.setVisible(true);
+				}
+			}
+		};
+		LinkThread.start();
+		
+	}*/
+	
