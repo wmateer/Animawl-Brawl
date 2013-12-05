780489fa75de5edf8105d37d92fac7b4d71d2b23
Will Mateer <wmateer@umail.ucsb.edu>
2013-12-04 20:25:59 -0800

    Added Anthony's changes to avoid merge conflict

--- Swing_GUI_TEST/Animawls_SWING/src/gui_WindowBuilder_TEST/GUI/GameResults_Screen.java
+++ Swing_GUI_TEST/Animawls_SWING/src/gui_WindowBuilder_TEST/GUI/GameResults_Screen.java
@@ -4,8 +4,11 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import java.awt.*;
+
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
+
+import GameEngine.User;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -13,77 +16,129 @@ import java.awt.image.BufferedImage;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
-import GameEngine.*;
+
 
 public class GameResults_Screen extends JPanel{
 
 	private JFrame parentFrame;
-	private JTextField txtTitle;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
 	
-	public GameResults_Screen(JFrame masterFrame,User currentUser, String gameWinner) {
+	private BufferedImage resizeImage(BufferedImage originalImage, int width, int height, int type) throws IOException {  
+        BufferedImage resizedImage = new BufferedImage(width, height, type);  
+        Graphics2D g = resizedImage.createGraphics();  
+        g.drawImage(originalImage, 0, 0, width, height, null);  
+        g.dispose();  
+        return resizedImage;  
+    }  
+	
+	public GameResults_Screen(JFrame masterFrame, User user, String gameWinner,double lvl0, double lvl1, double lvl2) {
 		parentFrame = masterFrame;
 		setBackground(Color.gray);
 		setLayout(null);
 		
-		txtTitle = new JTextField();
-		txtTitle.setText("TITLE");
-		txtTitle.setBounds(376, 98, 134, 28);
-		add(txtTitle);
-		txtTitle.setColumns(10);
+		//get different between animals lvls after battle and before
+		double lvld0 = user.getAnimalAtIndex(0).getLvl() - lvl0;
+		double lvld1 = user.getAnimalAtIndex(1).getLvl() - lvl1;
+		double lvld2 = user.getAnimalAtIndex(2).getLvl() - lvl2;
 		
-		textField = new JTextField();
-		textField.setBounds(240, 218, 134, 28);
-		add(textField);
-		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(240, 279, 134, 28);
-		add(textField_1);
-		textField_1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(240, 344, 134, 28);
-		add(textField_2);
-		textField_2.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(60, 218, 134, 28);
-		add(textField_3);
-		textField_3.setColumns(10);
+		JLabel title = new JLabel("Winner has defeated the loser!");
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Lao Sangam MN", Font.PLAIN, 24));
+		title.setBounds(85, 55, 730, 59);
+		add(title);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(60, 279, 134, 28);
-		add(textField_4);
-		textField_4.setColumns(10);
+		JLabel animalPic0 = new JLabel("New label");
+		try {
+			BufferedImage unsizedAnimalPicture = ImageIO.read(new File(user.getAnimalAtIndex(0).imgPath));
+			BufferedImage sizedAnimalPicture = resizeImage(unsizedAnimalPicture,200,200, unsizedAnimalPicture.getType());
+			animalPic0.setIcon(new ImageIcon(sizedAnimalPicture));
+			
+		} catch (IOException e1) {
+			 //TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		animalPic0.setHorizontalAlignment(SwingConstants.CENTER);
+		animalPic0.setBounds(74, 159, 200, 200);
+		add(animalPic0);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(60, 344, 134, 28);
-		add(textField_5);
-		textField_5.setColumns(10);
+		JLabel animalPic1 = new JLabel("New label");
+		try {
+			BufferedImage unsizedAnimalPicture = ImageIO.read(new File(user.getAnimalAtIndex(1).imgPath));
+			BufferedImage sizedAnimalPicture = resizeImage(unsizedAnimalPicture,200,200, unsizedAnimalPicture.getType());
+			animalPic1.setIcon(new ImageIcon(sizedAnimalPicture));
+			
+		} catch (IOException e1) {
+			 //TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		animalPic1.setHorizontalAlignment(SwingConstants.CENTER);
+		animalPic1.setBackground(new Color(238, 238, 238));
+		animalPic1.setBounds(350, 159, 200, 200);
+		add(animalPic1);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(60, 403, 134, 28);
-		add(textField_6);
-		textField_6.setColumns(10);
+		JLabel animalPic2 = new JLabel("New label");
+		try {
+			BufferedImage unsizedAnimalPicture = ImageIO.read(new File(user.getAnimalAtIndex(2).imgPath));
+			BufferedImage sizedAnimalPicture = resizeImage(unsizedAnimalPicture,200,200, unsizedAnimalPicture.getType());
+			animalPic2.setIcon(new ImageIcon(sizedAnimalPicture));
+			
+		} catch (IOException e1) {
+			 //TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		animalPic2.setHorizontalAlignment(SwingConstants.CENTER);
+		animalPic2.setBackground(new Color(238, 238, 238));
+		animalPic2.setBounds(629, 159, 200, 200);
+		add(animalPic2);
 		
-		textField_7 = new JTextField();
-		textField_7.setBounds(240, 403, 134, 28);
-		add(textField_7);
-		textField_7.setColumns(10);
+		JLabel lblNewLabel = new JLabel("Your Team");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(350, 97, 200, 39);
+		add(lblNewLabel);
 		
-		textField_8 = new JTextField();
-		textField_8.setBounds(376, 528, 134, 28);
-		add(textField_8);
-		textField_8.setColumns(10);
+		JLabel animalName0 = new JLabel(user.getAnimalAtIndex(0).getName());
+		animalName0.setHorizontalAlignment(SwingConstants.CENTER);
+		animalName0.setBounds(74, 140, 200, 16);
+		add(animalName0);
+		
+		JLabel animalName1 = new JLabel(user.getAnimalAtIndex(1).getName());
+		animalName1.setHorizontalAlignment(SwingConstants.CENTER);
+		animalName1.setBounds(350, 140, 200, 16);
+		add(animalName1);
+		
+		JLabel animalName2 = new JLabel(user.getAnimalAtIndex(2).getName());
+		animalName2.setHorizontalAlignment(SwingConstants.CENTER);
+		animalName2.setBounds(629, 140, 200, 16);
+		add(animalName2);
+		
+		JLabel lvlPrompt0 = new JLabel("New label");
+		if(lvld0 == 0){
+			lvlPrompt0.setText("Level is "+(user.getAnimalAtIndex(0).getLvl()));		
+		}else{
+			lvlPrompt0.setText("Leveled up by " +lvld0);
+		}
+		lvlPrompt0.setBounds(96, 383, 162, 16);
+		add(lvlPrompt0);
+		
+		JLabel lvlPrompt1 = new JLabel("New label");
+		if(lvld1 == 0){
+			lvlPrompt1.setText("Level is "+(user.getAnimalAtIndex(1).getLvl()));		
+		}else{
+			lvlPrompt1.setText("Leveled up by " +lvld1);
+		}
+		lvlPrompt1.setBounds(350, 383, 200, 16);
+		add(lvlPrompt1);
+		
+		JLabel lvlPrompt2 = new JLabel("New label");
+		if(lvld2 == 0){
+			lvlPrompt2.setText("Level is "+(user.getAnimalAtIndex(2).getLvl()));		
+		}else{
+			lvlPrompt2.setText("Leveled up by "+lvld2);
+		}
+		lvlPrompt2.setBounds(629, 383, 200, 16);
+		add(lvlPrompt2);
 		parentFrame.setLocationRelativeTo(null);
 		parentFrame.setSize(900, 600);
 		parentFrame.setVisible(true);
